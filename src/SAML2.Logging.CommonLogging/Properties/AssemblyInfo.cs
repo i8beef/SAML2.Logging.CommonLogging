4be3c697906bce195b2c9310687757e81daec274
Michael Hallock <i8beef@users.noreply.github.com>
2013-10-01 12:32:53 -0400

    Fixed the assembly info.

--- src/SAML2.Logging.CommonLogging/Properties/AssemblyInfo.cs
+++ src/SAML2.Logging.CommonLogging/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SAML2.Loggin.CommonLogging")]
+[assembly: AssemblyTitle("SAML2.Logging.CommonLogging")]
 [assembly: AssemblyDescription("A logging provider for SAML2 based on Common.Logging")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Michael Hallock")]
-[assembly: AssemblyProduct("SAML2.Loggin.CommonLogging")]
+[assembly: AssemblyProduct("SAML2.Logging.CommonLogging")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
